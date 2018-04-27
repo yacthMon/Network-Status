ef5405e57b384098f1d2d62259f3b1b5877c64e4
yacth_Mon <yacthmon@gmail.com>
2018-04-27 15:42:37 +0700

    Add getting config data

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const Monitor = require('./model/monitor');
+const network = require('network');
 let monitor = new Monitor();
+
+network.get_active_interface(function(err, interface) {
+  if(err) monitor.error(err);
+  monitor.setConfig({privateIP:interface.ip_address, gateway:interface.gateway_ip, dns:'8.8.8.8'})
+})
+network.get_public_ip(function(err, ip) {
+  if(err) monitor.error(err);
+  monitor.setConfig({publicIP:ip});
+})
